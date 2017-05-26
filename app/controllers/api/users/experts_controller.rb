6c81b52a7cf787f2a9503a71d8847a4a6785cc55
iyanski <me@iantusil.com>
2017-05-26 17:35:58 +0800

    Show resent invitation if expert has not logged in yet

--- app/controllers/api/users/experts_controller.rb
+++ app/controllers/api/users/experts_controller.rb
@@ -38,7 +38,9 @@ class Api::Users::ExpertsController < Api::UsersController
   def resend_invitation
     password = SecureRandom.hex(8)
     @expert = Expert.find(params[:id])
-    if UserMailer.send_expert_registration_information(@expert, password, "http://#{request.host_with_port}/experts/sign_in").deliver
+    @expert.password = password
+    if @expert.save
+      UserMailer.send_expert_registration_information(@expert, password, "http://#{request.host_with_port}/experts/sign_in").deliver
       render :create
     else
       render json: {error: "Failed to send invitation"}, status: 401
--- app/views/api/users/experts/index.rabl
+++ app/views/api/users/experts/index.rabl
@@ -1,2 +1,2 @@
 collection @experts
-attributes :id, :email, :first_name, :last_name, :created_at, :updated_at, :avatar
+attributes :id, :email, :first_name, :last_name, :created_at, :updated_at, :avatar, :sign_in_count
